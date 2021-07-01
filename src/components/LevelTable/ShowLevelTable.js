8fb95f35c4cd06b441d984b320a96a2da7a4f8f6
phamngocthai <ngocthaipham99@gmail.com>
2021-07-02 01:36:30 +0700

    push

--- src/components/LevelTable/ShowLevelTable.js
+++ src/components/LevelTable/ShowLevelTable.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Level from "./levelApi";
 import AddLevelForm from "./AddLevelForm";
 import EditLevelForm from "./EditLevelForm";
--- src/components/WordTable/ShowWordTable.js
+++ src/components/WordTable/ShowWordTable.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Word from ".././WordTable/wordApi";
 import AddWordForm from "./AddWordForm";
 import EditWordForm from "./EditWordForm";
